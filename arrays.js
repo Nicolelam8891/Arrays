f88a2dbb4fb4f820b88ccd9b8722b3763fc3be81
Nicole Lam <Nicolelam8891@gmail.com>
2023-05-16 22:38:29 -0600

    Add a method to each array

--- arrays.js
+++ arrays.js
@@ -1,3 +1,15 @@
 var names = ["Nicole", "John", "Jadalynn", "Devon"];
+/*This will remove the last element from an array & return that element. Devon will be removed*/
+names.pop();
+console.log(names);
+
 var numbers = [1, 2, 3, 4]; 
+/*This will add the specified elements to the end of an array & return the new lenght of the array.*/
+numbers.push(60);
+console.log(numbers);
+
 var pregnant = [true, false, false, false]; 
+/*This will remove the 1st element from an array & return that removed element. True will be removed*/ 
+pregnant.shift();
+console.log(pregnant);
+
