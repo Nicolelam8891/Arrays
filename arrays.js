11f3d1173fdc0a22ea8a6c36d2616f86c692a078
Nicole Lam <Nicolelam8891@gmail.com>
2023-05-16 22:42:16 -0600

    Add explanations about index positions for each array

--- arrays.js
+++ arrays.js
@@ -1,15 +1,20 @@
 var names = ["Nicole", "John", "Jadalynn", "Devon"];
-/*This will remove the last element from an array & return that element. Devon will be removed*/
+/*This will remove the last element from an array & return that element. 
+Devon will be removed*/
 names.pop();
 console.log(names);
+/*"Nicole" is index position 0. 
+"Devon" is index position 3*/
 
 var numbers = [1, 2, 3, 4]; 
 /*This will add the specified elements to the end of an array & return the new lenght of the array.*/
 numbers.push(60);
 console.log(numbers);
+//Number 1 is index position 0
 
 var pregnant = [true, false, false, false]; 
 /*This will remove the 1st element from an array & return that removed element. True will be removed*/ 
 pregnant.shift();
 console.log(pregnant);
+//True is index position 0 and false is index position 1, 2 and 3.
 
